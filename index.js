63b39ac002cad790e68a628dc99870bcf919c198
luzapedreira <luza.freitas@gmail.com>
2023-12-07 11:29:53 -0300

    oficina

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
+const { match } = require('assert');
 const fs = require('fs');
 
 function mdLinks(filePath) {
-  return new Promise(function(resolve, reject) => {
+  return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf-8', (err, data) => {
       if (err) reject(err);
-      
-      resolve(data);
+      const pattern = /\[([^\]]+)\[\((https?[^)]+)\)/g;
+      const matches = [...data.matchAll(pattern)];
+      const links = matches.map(match => {
+        return {
+          href: match[2],
+          text: match[1],
+          file: filePath
+        }
+      })
     });
   });
 }
 
-mdLinks('/README.md').then(result => console.log(result))
+mdLinks('./README.md').then((result) => console.log(result));
 
-module.exports = {mdLinks}
+module.exports = { mdLinks };
